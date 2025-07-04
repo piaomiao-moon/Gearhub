cbcb0f25b47ead7675a36538bf8aabe87706ea78
PIAOMIAOMOON\liuhu <huayang.liu@boounion.com>
2025-07-04 14:58:02 +0800

    12

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -7,7 +7,7 @@ const color = useState('color', () => colors[Math.floor(Math.random() * colors.l
 <template>
   <div class="centered">
     <h1 :style="{ color }">
-      {{ runtimeConfig.public.helloText11 }}
+      {{ runtimeConfig.public.helloText }}
     </h1>
     <NuxtLink
       to="/"
